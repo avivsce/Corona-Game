021ca5c581d1cdcc313508373aa458ee4fb45339
avivsce <93071437+avivsce@users.noreply.github.com>
2021-12-10 09:01:01 +0200

    Login
    update login with open windows for info

--- Login.py
+++ Login.py
@@ -7,11 +7,16 @@ from firebase_admin import db
 from tkinter import *
 import tkinter as tk
 from firebase_admin import credentials
+
 cred_obj = firebase_admin.credentials.Certificate('f.json')
+#cred_obj = credentials.Certificate('f.json')
+
+#firebase_admin.initialize_app(cred_obj)
+
 default_app = firebase_admin.initialize_app(cred_obj, {
     'databaseURL': 'https://coronagame-151a7-default-rtdb.firebaseio.com'
 })
-#firebase_admin.initialize_app(cred_obj)
+
 
 
 # Login function
@@ -31,28 +36,71 @@ def login(email=None, password=None):
     #    print("Invalid email or password")
     return
 
+def SignUpInfo():
+    frame = tk.Tk()
+    frame.title("Register Info")
+    frame.geometry('400x400')
+
+    name = Label(frame,text="email: ")
+    name.pack()
+    namevalue = StringVar
+    nameentry = Entry(frame, textvariable=namevalue)
+    nameentry.pack()
+
+    password = Label(frame, text="passowrd: ")
+    password.pack()
+    passvalue = StringVar
+    passentry = Entry(frame, textvariable=passvalue)
+    passentry.pack()
+
+    #Submit Button
+    Label(text="").pack()
+    Button(frame, text="submit", command=lambda: signup(nameentry, passentry)).pack()
+
+    #Back Button
+    Label(text="").pack()
+    button = tk.Button(frame,text='Back',command=lambda: frame.destroy())
+    button.pack()
+
+    return namevalue,passvalue
+    frame.mainloop()
 
 # Signup Function
 
-def signup():
+def signup(email, password):
     print("Sign up...")
-    email = input("Enter email: ")
-    password = input("Enter password: ")
+    #print(email.get())
+    #print(password.get())
     try:
-        user = auth.create_user(email=email, password=password)
+        user = auth.create_user(email=email.get(), password=password.get())
     except:
         print("Email already exists")
         return
+    frame = tk.Tk()
+    frame.title("Question")
+    frame.geometry('400x400')
+    name = Label(frame, text="Do you want to login? ")
+    name.pack()
+    button = tk.Button(frame, text='Yes', command=login)
+    button.pack()
+    button = tk.Button(frame, text='No', command=lambda: frame.destroy())
+    button.pack()
+
+
+    '''
     ask = input("Do you want to login?[y/n]")
     if ask == 'y':
         login()
     return
+    '''
 
 
 def fu():
     ref = db.reference("/Users")
     users = ref.get()
     print(users)
+
+
 
 # Main
 def main_screen():
@@ -61,12 +109,14 @@ def main_screen():
     screen.title("Game Start")
     Label(text="Game Start", bg="grey", font=("calibri", 13)).pack()
     Label(text="").pack()
-    Button(text="Login", command=fu).pack()
+    Button(screen,text="Login", command=fu).pack()
     Label(text="").pack()
-    Button(text="Register", command=signup).pack()
+    Button(screen,text="Register", command=SignUpInfo).pack()
     Label(text="").pack()
-    Button(text="Start").pack()
-
+    Button(screen,text="Start").pack()
+    Label(text="").pack()
+    button = tk.Button(screen, text='EXIT', command=lambda: screen.destroy())
+    button.pack()
     screen.mainloop()
 
 
