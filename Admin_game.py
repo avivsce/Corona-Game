f77548ec161c2496e434f13093d5bd2e82ad7f76
ITAIDAMRI <93492233+ITAIDAMRI@users.noreply.github.com>
2021-12-23 18:56:14 +0200

    Add files via upload
    button

--- Admin_game.py
+++ Admin_game.py
@@ -1,5 +1,7 @@
 import pygame, sys
 import random
+from tkinter import *
+import tkinter as tk
 import time
 
 pygame.init()
@@ -13,7 +15,7 @@ blue = (0, 0, 255)
 corona = (150, 0, 0)
 healer = (0, 255, 255)
 clock = pygame.time.Clock()
-display_width, display_height = 1500, 1100
+display_width, display_height = 1300, 800
 screen = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption("Admin game")
 screen.fill(white)
@@ -21,6 +23,7 @@ balls = []
 mouseClick_x = 0
 mouseClick_y = 0
 points = 0
+my_font = pygame.font.SysFont("Arial", 20)
 
 class Circle():
     def __init__(self, x_cord, y_cord, x_vel, y_vel):
@@ -40,13 +43,13 @@ class Circle():
         self.player_pos_y += self.speed_y
 
     def bouncing_rect(self):
-        if self.player_pos_x < 20:
+        if self.player_pos_x < 100:
             self.speed_x *= -1
-        if self.player_pos_x > 1500:
+        if self.player_pos_x > 1285:
             self.speed_x *= -1
-        if self.player_pos_y < 10:
+        if self.player_pos_y < 15:
             self.speed_y *= -1
-        if self.player_pos_y > 1050:
+        if self.player_pos_y > 789:
             self.speed_y *= -1
 
 
@@ -86,15 +89,32 @@ def mousecheck(array_balls):
                 points += 1
                 print(points)
 
+def change_to_True(flag):
+    #global flag
+    flag = True
+    return flag
+
+
+
+
+
+
 
 def game():
-    balls.append(Circle(random.randint(100, 1350), random.randint(50, 1000), 4, 4))
+
+    flag = False
+    if flag == True:
+        balls[1].player_color = healer
+
+    balls.append(Circle(random.randint(100, 1150), random.randint(50, 650), 4, 4))
     balls[0].player_color = corona
-    balls.append(Circle(random.randint(100, 1350), random.randint(50, 1000), 4, 4))
-    balls[1].player_color = healer
+    balls.append(Circle(random.randint(100, 1150), random.randint(50, 650), 4, 4))
+    #balls[1].player_color = healer
     for i in range(25):
         balls.append(
-            Circle(random.randint(150, 1350), random.randint(50, 1000), random.randint(-4, 4), random.randint(-4, 4)))
+            Circle(random.randint(150, 1150), random.randint(50, 650), random.randint(-4, 4), random.randint(-4, 4)))
+
+    text = my_font.render('doc', True, red)
 
     while True:
         for event in pygame.event.get():
@@ -102,7 +122,13 @@ def game():
                 return
             if event.type == pygame.MOUSEBUTTONUP:
                 mousecheck(balls)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                print(mouseClick_x,mouseClick_y)
+                if 5<= mouseClick_x<=55  and 50 <=mouseClick_y <= 80 :
+                    balls[1].player_color = healer
+
         screen.fill(white)
+        screen.blit(text,(15,50))
         colide(balls)
         [Circle.bouncing_rect() for Circle in balls]
         [Circle.update() for Circle in balls]
